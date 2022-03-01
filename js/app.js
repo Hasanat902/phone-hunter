c59938ee625486b1c16412517c7e3863156bce7f
Hasanat902 <abulhasanat902@gmail.com>
2022-03-01 11:59:22 +0600

    display phone in site

--- js/app.js
+++ js/app.js
@@ -1,8 +1,42 @@
 const loadPhones = () => {
-    const searchText = document.getElementById("input-field").value;
+    const inputField = document.getElementById("input-field");
+    const searchText = inputField.value;
     
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.data))
+        .then(data => displayPhones(data.data))
+
+    inputField.value = '';
+}
+
+const displayPhones = phones => {
+    
+    const phoneContainer = document.getElementById("phone");
+    phoneContainer.textContent = '';
+    const showTwentyPhone = phones.slice(0,20);
+        
+    if(showTwentyPhone == ''){
+        alert("Phone not found");
+    }
+    else{
+        showTwentyPhone.forEach(phone => {
+            console.log(phone);
+            const div = document.createElement("div");
+            div.classList.add("col-lg-4");
+            div.classList.add("gy-5");
+            div.innerHTML = `
+                <div class="card" style="width: 18rem;">
+                    <img src="${phone.image}" class="card-img-top h-50" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Name: ${phone.phone_name}</h5>
+                        <h6 class="card-title">Brand: ${phone.brand}</h6>
+                        <button href="#" class="btn btn-primary">Details</button>
+                    </div>
+                </div>
+            `;
+            phoneContainer.appendChild(div);
+       })
+    }
+    
 }
