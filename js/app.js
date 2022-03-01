eb0a9e91bb377c35bc46bf737bdf593daee4a885
Hasanat902 <abulhasanat902@gmail.com>
2022-03-01 11:04:16 +0600

    load phones from api

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 const loadPhones = () => {
     const searchText = document.getElementById("input-field").value;
     
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data.data))
 }
